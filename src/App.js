a02e252290fdb5fcf3288b731e4c695210e2fda6
Elena Vengerova <hizeretta@gmail.com>
2024-02-03 23:06:16 +0200

    added main, products, brands pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import MainPage from './pages/MainPage';
+import BrandsPage from './pages/BrandsPages';
+import ProductsPage from './pages/ProductsPage';
+import ScrollToTop from './helpers/scrollToTop/scrolToTop';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ScrollToTop />
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/brands" element={<BrandsPage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
